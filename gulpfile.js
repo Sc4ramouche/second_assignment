fbb22c5048029761501ef94ec416a014a901183b
Sc4ramouche <vladislav.kovechenkov@gmail.com>
2018-01-29 08:46:53 +0300

    completed some sections

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,12 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 
-gulp.task('default', function() {
-  console.log(10);
+gulp.task('sass', function() {
+  gulp.src('./assets/scss/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./public/css'));
+});
+
+gulp.task('sass:watch', function() {
+  gulp.watch('./assets/scss/**/*.scss', ['sass']);
 });
